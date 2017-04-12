d535d86ece486272cc80c67dedfece5ed1af66a9
cale312 <zikisacalesogaxa@gmail.com>
2017-04-12 18:01:13 +0200

    initial comit

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,1 +1,9 @@
+const greet = require('../greet.js');
 const assert = require('assert');
+
+describe('The greet function', function () {
+    'use strict';
+    it('Should greet Zikisa correctly', function () {
+        assert.equal('Hello, Zikisa', greet('Zikisa'));
+    });
+});
